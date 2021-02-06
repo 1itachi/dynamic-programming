4238c3218ef5d5035cc144a97962197aaea1f919
deepak <kumar.de@husky.neu.edu>
2021-02-06 18:21:30 -0500

    best sum

---
+++ Memoization/BestSum/bestSumDP.js
@@ -0,0 +1,34 @@
+// Write a function `bestSum(target, numbers)` that takes in a targetSum and an array of numbers as arguments
+// The function should return an array containing the shortest combination of numbers that add up to exactly the traget sum.
+// if there is a tie, return one of the shortest.
+
+const bestSum=(target , numbers, memo={})=>{
+    if(target in memo) return memo[target];
+    if(target===0) return [];
+    if(target<0) return null;
+
+    let shortestCombination = null;
+
+     for(let num of numbers){
+        let remainder = target-num;
+        let remainderCombination = bestSum(remainder, numbers, memo);
+        if(remainderCombination !== null){
+            let combination = [...remainderCombination, num];
+            if(shortestCombination===null || combination.length < shortestCombination.length){
+                shortestCombination = combination;
+            }
+
+        }
+     }
+     
+    memo[target] = shortestCombination;
+    return shortestCombination;
+}
+
+console.log(bestSum(7,[5,3,4,7])) //7
+console.log(bestSum(8,[2,3,5])) //[3,5]
+console.log(bestSum(8,[1,4,5])) //[4,4]
+console.log(bestSum(100,[1,2,5,25])) //[25,25,25,25]
+
+//time compelxity: O(m^2 * n)   m-target sum , n- numbers.length
+//space complexity: O(m^2)
