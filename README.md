add1ef66e5217bbcc74fae5f9aae12c292fe2b38
deepak <kumar.de@husky.neu.edu>
2021-03-20 23:49:12 -0400

    removed files , added to markdown && canconstruct-tabulation :fire:

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # dynamic-programming
 Dynamic programming problems
 
-this was updated to test the --force command
+## Memoization
+1. Make it work
+    a. Visualize the problem as a tree (each edge should decrease the problem)
+    b. implement the tree using a recursion
+    c. test it
+
+2. Make it efficient
+    a. Add a memo object
+    b. add a base case to return memo values
+    c. store return values into the memo
+
+## Tabulation
+1. Visualize the problem as a table
+2. Size the table based on the inputs
+3. initialize the table with default values
+4. seed the trivial answer into the table
+5. Iterate through the table
+6. Fill further positions based on the current position
---
+++ Tabulation/Construct/canConstruct.js
@@ -0,0 +1,52 @@
+const { tsEnumDeclaration } = require("@babel/types")
+
+const canConstruct=(word, arrWords)=>{ 
+
+    let tempArray = Array(word.length+1).fill(false)
+    tempArray[0] = true // "" is always possible
+
+    for(let i=0; i<= word.length; i++){
+
+        if(tempArray[i] === true){
+
+            arrWords.forEach(element => {
+                //slice => from position to position(excluding)
+                if(word.slice(i, i+ element.length)===element){
+
+                    tempArray[i + element.length] = true
+
+                }
+
+
+            });
+
+
+        }
+
+    }
+
+
+    return tempArray[word.length]
+
+}
+
+
+
+
+
+
+
+
+console.log(canConstruct('abcdef', ['abc','abc', 'cd', 'def', 'abcd'])) //=> true
+console.log(canConstruct('skateboard', ['bo','rd', 't', 'ska', 'sk', 'boar'])) //=> false
+console.log(canConstruct('enterapotentpot', ['a','p', 'ent', 'enter', 'ot', 'o', "t"])) //=> true
+console.log(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e','ee', 'eee', 'eeee', 'eeeee', 'eeeeee'])) //=> false
+
+//time complexity: O(m^2 * n)
+//space complexity: O(m)
+
+
+//for first test case the array will look somthing like below
+//  0    1    2   3    4    5   6   
+//| T |  F |  F | F  | F |  F | F  |
+//  a    b    c   d    e    f
